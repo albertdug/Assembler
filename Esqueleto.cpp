de7725a7d9b1bacce1bb57689b69eba2e5c17409
Albert Duran <albertrdg@gmail.com>
2015-07-15 20:27:12 -0400

    Partes finales del proyecto

---
+++ Esqueleto.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+char[9] entrada;
+int[7] codigosOp;
+int[4] codigosVar;
+int[4] codigosReg;
+int longitudEntrada;
+int nOp;
+int nVar;
+int nReg;
+/*
+	codOp, tipoOp, op1,op2 son registros
+*/
+string clr="CLR";
+//etc
+string a="A";
+//etc
+string r0="R0";
+//etc
+
+void CARGARCODIGOSOP(int[] arreglo, int longitud){
+	
+}
+void CARGARCODIGOSVAR(int[] arreglo, int longitud){
+
+}
+void CARGARCODIGOSREG(int[] arreglo, int longitud){
+
+}
+
+//Alejandro
+void CADENAANUMERO(char[] cadena, int inicio, int fin, int numero){
+
+}
+void DIVIDIRCADENA(char[] cadena, int longitud, int codOp, int tipoOp, int op1, int op2){
+
+}
+
+//Albert
+void VALIDARCADENA(char[] cadena, int longitud, bool resultado){
+
+}
+void VALIDARCODIGOS(int[] arreglo, int longitud, int valor, bool resultado){
+
+}
+
+//void TRANSFORMAROP(int nOp);
+//void TRANSFORMARVAR(int nVar);
+//void TRANSFORMARREG(int nReg);
+
+//Rosa
+void TRADUCIR(int[] arreglo, int longitud, int dato, int resultado){
+
+}
+
+int main(){
+
+}
